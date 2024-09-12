2a082016fd44398448a9cae6349f43c60c5f2f98
dansoon7 <kkhy0423@gmail.com>
2024-09-12 12:47:17 +0900

    Create main.js

---
+++ main.js
@@ -0,0 +1,35 @@
+
+(function () {
+const template = document.createElement('template')
+template.innerHTML = `
+<style>
+</style>
+<div id="root" style="width: 100%; height: 100%;">
+Hello Custom Widget >>> KHY
+</div>
+`
+class Main extends HTMLElement {
+constructor () {
+super()
+this._shadowRoot = this.attachShadow({ mode: 'open' })
+this._shadowRoot.appendChild(template.content.cloneNode(true))
+this._root = this._shadowRoot.getElementById('root')
+}
+}
+customElements.define('com-sap-sac-exercise-y001-main', Main)
+
+onCustomWidgetResize(width,height)
+  {
+     this.render()
+  }
+
+onCustomWidgetAfterUpdate(changedProps)
+  {
+    
+  }
+onCustomWidgetDestory(){
+}
+ reder(){
+   this._root.textContent='Hello Custom Widget clientWith:${this.clentWidth}, cleintHeight:${this.clientHeight}'
+ }
+})()
